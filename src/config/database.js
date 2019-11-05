742087f599e27d3b9e950bb64bbc73c266d61211
Rafael V. dos Santos <rvsantos00@gmail.com>
2019-11-04 23:57:32 -0300

    fix errors on src/config/database.js

--- src/config/database.js
+++ src/config/database.js
@@ -1,9 +1,9 @@
-module.export = {
+module.exports = {
+  dialect: 'postgres',
   username: 'postgres',
   password: 'docker',
   database: 'meetapp',
   host: '172.17.0.2',
-  dialect: 'postges',
   define: {
     timestamps: true,
     underscored: true,
