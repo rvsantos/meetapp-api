7b624855d51bc0052b2354d0c65767dc13ada17f
Rafael V. dos Santos <rvsantos00@gmail.com>
2019-11-05 00:36:43 -0300

    fix errors on users register

--- src/app/controllers/UserController.js
+++ src/app/controllers/UserController.js
@@ -1,14 +1,29 @@
+import * as Yup from 'yup';
 import User from '../models/user';
 
 class UserController {
   async store(req, res) {
+    const schema = Yup.object().shape({
+      name: Yup.string().required(),
+      email: Yup.string()
+        .email()
+        .required(),
+      password_hash: Yup.string()
+        .required()
+        .min(6)
+    });
+
+    if (!(await schema.isValid(req.body))) {
+      return res.status(400).json({ error: 'Validate fails' });
+    }
+
     const userExists = await User.findOne({ where: { email: req.body.email } });
 
     if (userExists) {
       return res.status(400).json({ error: 'User already exists' });
     }
 
-    const { id, name, email } = req.body;
+    const { id, name, email } = await User.create(req.body);
 
     return res.status(201).json({
       id,
