719560aa81ba2a30b1b78e21851782cb9e35d1b7
Rafael V. dos Santos <rvsantos00@gmail.com>
2019-11-04 23:21:54 -0300

    Eslint fix

--- src/routes.js
+++ src/routes.js
@@ -1,8 +1,8 @@
-import { Router } from "express";
+import { Router } from 'express';
 
 const routes = new Router();
 
-routes.get("/", (req, res) => {
+routes.get('/', (req, res) => {
   return res.json({ ok: true });
 });
 
