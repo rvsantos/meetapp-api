a1903cdcb8bff6b6d7e5f1488d18557e828d0ca6
Rafael V. dos Santos <rvsantos00@gmail.com>
2019-11-21 01:00:08 -0300

    Adding store action to FileController

--- src/routes.js
+++ src/routes.js
@@ -1,17 +1,29 @@
 import { Router } from 'express';
+import multer from 'multer';
+import multerConfig from './config/multer';
 
 import UserController from './app/controllers/UserController';
 import SessionController from './app/controllers/SessionController';
+import FileController from './app/controllers/FileController';
 
 import authMiddlewares from './app/middlewares/auth';
 
 const routes = new Router();
+const upload = multer(multerConfig);
 
+// User
 routes.post('/users', UserController.store);
+
+// Session
 routes.post('/sessions', SessionController.store);
 
+// Middleware
 routes.use(authMiddlewares);
 
+//  User authenticate
 routes.put('/users', UserController.update);
+
+// File
+routes.post('/files', upload.single('file'), FileController.store);
 
 export default routes;
