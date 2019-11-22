d460a7f77927e11732bfb65d1550cc1b8787f8e8
Rafael V. dos Santos <rvsantos00@gmail.com>
2019-11-21 21:58:25 -0300

    Adding store action to MeetupController

---
+++ src/app/controllers/MeetupController.js
@@ -0,0 +1,42 @@
+import * as Yup from 'yup';
+import Meetup from '../models/Meetup';
+import File from '../models/File';
+
+class MeetupController {
+  async store(req, res) {
+    const schema = Yup.object().shape({
+      title: Yup.string().required(),
+      description: Yup.string().required(),
+      location: Yup.string().required(),
+      date: Yup.date().required(),
+      file_id: Yup.number()
+        .positive()
+        .required()
+    });
+
+    if (!(await schema.isValid(req.body))) {
+      return res.status(400).json({ error: 'Validation fails' });
+    }
+
+    const { title, description, location, date, file_id } = req.body;
+
+    const checkFileExists = await File.findByPk(file_id);
+
+    if (!checkFileExists) {
+      return res.status(400).json({ errors: 'File does exists' });
+    }
+
+    const meetup = await Meetup.create({
+      title,
+      description,
+      location,
+      date,
+      file_id,
+      user_id: req.userId
+    });
+
+    return res.json(meetup);
+  }
+}
+
+export default new MeetupController();
--- src/routes.js
+++ src/routes.js
@@ -5,6 +5,7 @@ import multerConfig from './config/multer';
 import UserController from './app/controllers/UserController';
 import SessionController from './app/controllers/SessionController';
 import FileController from './app/controllers/FileController';
+import MeetupController from './app/controllers/MeetupController';
 
 import authMiddlewares from './app/middlewares/auth';
 
@@ -25,5 +26,8 @@ routes.put('/users', UserController.update);
 
 // File
 routes.post('/files', upload.single('file'), FileController.store);
+
+// Meetup
+routes.post('/meetups', MeetupController.store);
 
 export default routes;
